aad41b892ef94d249396cc8a5a2b31d6c96fd5b7
robotscissors <cjbazin@gmail.com>
2018-01-04 21:51:34 -0800

    complete implementation

--- complexity-complex-problems/tsp_greed.rb
+++ complexity-complex-problems/tsp_greed.rb
@@ -2,30 +2,24 @@ require_relative 'city_node'
 
 def nearest_possible_neighbor(current_city)
 
-  puts "going in current City: #{current_city.name}"
-  # WHILE current_city.visited IS FALSE
   while !current_city.visited do
-  #   SET neighbor_cities TO current_city.neighbors
+    puts "Going in current City: #{current_city.name}"
+    found_flag = false
     neighbor_cities = current_city.neighbors
-    puts " Neighbor Cities for: #{current_city.name} => #{neighbor_cities}"
-  #   SET next_city = neighbor_cities[0]
     next_city = neighbor_cities[0]
-  #   FOR current_neighbor IN neighbor_cities
     neighbor_cities.each do |current_neighbor|
-  #     IF current_neighbor.distance < next_city.distance
-      puts "Current_neighbor #{current_neighbor.city.name} distance: #{current_neighbor.distance}"
       if current_neighbor.distance < next_city.distance
-  #       ASSIGN current_neighbor TO next_city
         next_city = current_neighbor.city
-        puts "The closer City is: #{next_city.name}"
-  #     END IF
+        found_flag = true
       end
-  #   END FOR
     end
-  # SET current_city.visited to TRUE
     current_city.visited = true
-  # ASSIGN next_city TO current_city
-    current_city = next_city
+    puts "Current_city is #{current_city.name}" if current_city.visited
+    if found_flag
+      current_city = next_city
+    else
+      current_city = next_city.city
+    end
     puts "The next city is: #{current_city.name}"
   end # END WHILE
 
